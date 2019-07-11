0e5fa3f76c4e0f36828dd16c3ff951a89b2b797c
mateuszac <mateuszac@tlen.pl>
2019-07-11 17:00:03 +0200

    Changes in code to fulfill PEP8 standards using PyCharm IDE for first time

--- sandbox/command_line/demo_cmdl.py
+++ sandbox/command_line/demo_cmdl.py
@@ -3,29 +3,29 @@ import numpy
 
 
 class DemoCmdl(object):
-    """Manage demo of of showin command line parsing"""
+    """Manage demo of showing command line parsing"""
     def __init__(self):
-        self.parser = self.SetupCmdlineParser()
+        self.parser = self.setup_cmdline_parser()
         self.args = self.parser.parse_args()
 
-    def SetupCmdlineParser(self):
+    def setup_cmdline_parser(self):
         """Create parser for command line arguments"""
         parser = argparse.ArgumentParser(description=
-           'Demonstrate building and saving mesh for rectangle')
+                                         'Demonstrate building and saving mesh for rectangle')
         parser.add_argument('--nodes',
-                        dest='nodes_count',
-                        default=5,
-                        type=int,
-                        help='Number of nodes'
-                       )
+                            dest='nodes_count',
+                            default=5,
+                            type=int,
+                            help='Number of nodes'
+                            )
         return parser
 
-    def Run(self):
-        """Run demonstration of generaging simple mesh model"""
+    def run(self):
+        """Run demonstration of generating simple mesh model"""
         nodes = numpy.array(range(self.args.nodes_count))
         print('Nodes: ', nodes)
 
 
 if __name__ == '__main__':
     demo = DemoCmdl()
-    demo.Run()
+    demo.run()
